8a4b32f4daa83f55698193b949b64c84ab3120b3
jp928 <piaojingtai@gmail.com>
2021-07-28 23:11:26 +1000

    feat: call register api

--- lib/screens/get_start_screen.dart
+++ lib/screens/get_start_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:solo_traveller/futures/register_future.dart';
 import 'package:solo_traveller/widgets/round_gradient_button.dart';
 
 class GetStartScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  final _registerForm = GlobalKey<FormState>();
+
+  void _register(context) async {
+    final isValid = _registerForm.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+
+    bool result = await register(_emailController.text, _passwordController.text);
+
+    if (result) {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +38,7 @@ class GetStartScreen extends StatelessWidget {
           child: SingleChildScrollView(
               child: Container(
             // height: 500,
-            height: 600,
+            height: 720,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
@@ -40,55 +60,90 @@ class GetStartScreen extends StatelessWidget {
                           fontSize: 15, fontWeight: FontWeight.w400),
                     )),
                 Expanded(
-                    child: Padding(
+                  child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 25),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
-                            ),
+                    child: Form(
+                      key: _registerForm,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _emailController,
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return 'Please enter you email.';
+                              }
 
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
-                            ),
-                            hintText: 'Email'
-                          ),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
-                            ),
-                            hintText: 'Password',
-                          ),
-                          // scrollPadding: EdgeInsets.only(top: 100),
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
-                            ),
-                            hintText: 'Confirm password'),
-                        ),
+                              if (RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(text) == false) {
+                                return 'Please enter a valid email.';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
+                                ),
 
-                        RoundedGradientButton(
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
+                                ),
+                                hintText: 'Email'
+                            ),
+                            autocorrect: false,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return 'Please enter a password.';
+                              }
+
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
+                              ),
+                              hintText: 'Password',
+                            ),
+                            autocorrect: false,
+                            obscureText: true,
+                          ),
+                          TextFormField(
+                            controller: _confirmPasswordController,
+                            validator: (text) {
+                              if (text == null || text.isEmpty)
+                                return 'Please enter a password.';
+                              if (text != _confirmPasswordController.text)
+                                return 'Confirm password has to match.';
+
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: new BorderSide(color: Color.fromRGBO(218, 218, 236, 1))
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: new BorderSide(color: Color.fromRGBO(79, 152, 248, 1))
+                                ),
+                                hintText: 'Confirm password'
+                            ),
+                            autocorrect: false,
+                            obscureText: true,
+                          ),
+
+                          RoundedGradientButton(
                             buttonText: 'Register',
                             width: 300,
-                            onPressed: () {
-
-                        })
-                      ],
-                    ),
+                            onPressed: () => _register(context),
+                          ),
+                        ],
+                      ),
+                    )
                 ))
               ],
             ),
