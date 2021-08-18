34e70089f739fea2ec64d227ebebf7456175062d
Jing Piao <jing.piao@nectr.com.au>
2021-08-18 11:12:04 +1000

    feat: year picker

--- lib/screens/create_profile_screen.dart
+++ lib/screens/create_profile_screen.dart
@@ -4,24 +4,36 @@ import 'package:flutter/material.dart';
 import 'package:solo_traveller/widgets/outline_text_field.dart';
 import 'package:solo_traveller/widgets/years_picker.dart';
 
+import 'dart:developer';
 
-class CreateProfileScreen extends StatelessWidget {
+class CreateProfileScreen extends StatefulWidget {
+  const CreateProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateProfileScreenState createState() => _CreateProfileScreenState();
+}
+
+class _CreateProfileScreenState extends State<CreateProfileScreen> {
+  String selectedYear = 'Year of birth';
+
   @override
   Widget build(BuildContext context) {
     final TextEditingController _firstNameController = TextEditingController();
-    String selectedYear = 'Year of birth';
     final _createProfileForm = GlobalKey<FormState>();
 
     void _showYearsPicker() {
       showModalBottomSheet(
-          // backgroundColor: beachRed[50],
+        // backgroundColor: beachRed[50],
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(15.0)),
           ),
           context: context,
           builder: (context) => YearsPicker(
-            onChange: (year) => {
-              print(year)
+            initValue: selectedYear == 'Year of birth' ? null : selectedYear,
+            onChange: (year) {
+              setState(() {
+                selectedYear = year;
+              });
             },
           )).whenComplete(() {
         // setState(() {});
@@ -44,7 +56,7 @@ class CreateProfileScreen extends StatelessWidget {
                 },
                 child: SingleChildScrollView(
                   child: Container(
-                      // height: 500,
+                    // height: 500,
                       height: 800,
                       child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -71,38 +83,38 @@ class CreateProfileScreen extends StatelessWidget {
                             Expanded(
                                 child: Padding(
                                     padding:
-                                        EdgeInsets.symmetric(horizontal: 25),
+                                    EdgeInsets.symmetric(horizontal: 25),
                                     child: Form(
                                         key: _createProfileForm,
                                         child: Column(
                                             crossAxisAlignment:
-                                                CrossAxisAlignment.center,
+                                            CrossAxisAlignment.center,
                                             mainAxisAlignment:
-                                                MainAxisAlignment.spaceEvenly,
+                                            MainAxisAlignment.spaceEvenly,
                                             children: <Widget>[
                                               OutlineTextField(
                                                 controller:
-                                                    _firstNameController,
+                                                _firstNameController,
                                                 hintText: 'Your first name',
                                               ),
-                                               CountryCodePicker(
-                                                  onChanged: print,
-                                                  // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
-                                                  initialSelection: 'AU',
-                                                  // favorite: ['+39','FR'],
-                                                  // optional. Shows only country name and flag
-                                                  showCountryOnly: true,
-                                                  // optional. Shows only country name and flag when popup is closed.
-                                                  showOnlyCountryWhenClosed: true,
-                                                  // optional. aligns the flag and the Text left
-                                                  alignLeft: true,
-                                                  showFlag: false,
-                                                  showFlagDialog: true,
-                                                  textStyle: TextStyle(
+                                              CountryCodePicker(
+                                                onChanged: print,
+                                                // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                                                initialSelection: 'AU',
+                                                // favorite: ['+39','FR'],
+                                                // optional. Shows only country name and flag
+                                                showCountryOnly: true,
+                                                // optional. Shows only country name and flag when popup is closed.
+                                                showOnlyCountryWhenClosed: true,
+                                                // optional. aligns the flag and the Text left
+                                                alignLeft: true,
+                                                showFlag: false,
+                                                showFlagDialog: true,
+                                                textStyle: TextStyle(
                                                     fontSize: 16,
                                                     color: Color.fromRGBO(170, 175, 190, 1)
-                                                  ),
-                                                  builder: (countryCode) => Container(
+                                                ),
+                                                builder: (countryCode) => Container(
                                                     height: 56,
                                                     width: double.infinity,
                                                     alignment: Alignment.centerLeft,
@@ -115,26 +127,32 @@ class CreateProfileScreen extends StatelessWidget {
                                                         )
                                                     ),
                                                     child: countryCode == null ? Text('Country you\'re from') : Text(countryCode.name.toString())
-                                                  ),
-                                               ),
-                                                Container(
-                                                    height: 56,
-                                                    width: double.infinity,
-                                                    alignment: Alignment.centerLeft,
-                                                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                                                    decoration: BoxDecoration(
-                                                        borderRadius: BorderRadius.circular(6),
-                                                        border: Border.all(
-                                                          width: 1.6,
-                                                          color: Color.fromRGBO(218, 218, 236, 1),
-                                                        )
+                                                ),
+                                              ),
+                                              Container(
+                                                height: 56,
+                                                width: double.infinity,
+                                                alignment: Alignment.centerLeft,
+                                                // padding: const EdgeInsets.symmetric(horizontal: 2),
+                                                decoration: BoxDecoration(
+                                                    borderRadius: BorderRadius.circular(6),
+                                                    border: Border.all(
+                                                      width: 1.6,
+                                                      color: Color.fromRGBO(218, 218, 236, 1),
+                                                    )
+                                                ),
+                                                child: TextButton(
+                                                    child: Text(
+                                                      selectedYear,
+                                                      textAlign: TextAlign.left,
+                                                      style: TextStyle(
+                                                          color: Color.fromRGBO(182, 187, 200, 1)
+                                                      ),
                                                     ),
-                                                    child: TextButton(
-                                                      child: Text(selectedYear, textAlign: TextAlign.left),
-                                                      onPressed: () {
-                                                        _showYearsPicker();
-                                                      }),
-                                                  ),
+                                                    onPressed: () {
+                                                      _showYearsPicker();
+                                                    }),
+                                              ),
                                             ]))))
                           ])),
                 ))));
